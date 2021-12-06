4cbcf5e1defb2c8c6d9ac4c0dce08ec63a08bda1
beatrizhidalgom2001 <beatrizhidalgom2001@gmail.com>
2021-12-06 09:11:20 -0600

    OrderItems

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,3 +1,31 @@
 class OrderItem < ApplicationRecord
     belongs_to :order
+    has_many :products
+    validates :total, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
+    validates :quantity, numericality: true
+    before_create :set_total
+    validate :validate_quantity, :validate_product
+
+    # Custum validations
+    def validate_product
+        product = Product.find_by(id: product_id)
+        if product.active == false
+            errors.add(:product_id, "Can not add deactivate products.")
+        end
+    end
+
+    def validate_quantity
+        if quantity <= 0
+            errors.add(:quantity, "The quantity can not be less or equal to 0.")
+        end
+    end
+
+    private
+    # Intance methods
+    def set_total
+        product = Product.find_by(id: self.product_id)
+        product_price = product.price 
+        self.total = self.quantity * product_price
+    end
+
 end
